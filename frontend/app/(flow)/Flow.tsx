e36e4eda0f78ca7ae33554f8a78f6c12b93d95a5
Puttipong <voaiutersna@gmail.com>
2025-10-01 22:43:35 +0700

    AddFrontend

---
+++ frontend/app/(flow)/Flow.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useState, useCallback } from 'react';
+import {
+  ReactFlow,
+  addEdge,
+  applyNodeChanges,
+  applyEdgeChanges,
+  type Node,
+  type Edge,
+  type FitViewOptions,
+  type OnConnect,
+  type OnNodesChange,
+  type OnEdgesChange,
+  type OnNodeDrag,
+  type DefaultEdgeOptions,
+} from '@xyflow/react';
+import "@xyflow/react/dist/style.css";
+ 
+const initialNodes: Node[] = [
+  { id: '1', data: { label: 'Node 1' }, position: { x: 5, y: 5 } },
+  { id: '2', data: { label: 'Node 2' }, position: { x: 5, y: 100 } },
+];
+ 
+const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+ 
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
+ 
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+ 
+const onNodeDrag: OnNodeDrag = (_, node) => {
+  console.log('drag event', node.data);
+};
+ 
+export default function Flow() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+ 
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes],
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges],
+  );
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges],
+  );
+ 
+  return (
+    <div className='w-screen h-[100dvh] bg-white'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodeDrag={onNodeDrag}
+        fitView
+        fitViewOptions={fitViewOptions}
+        defaultEdgeOptions={defaultEdgeOptions}
+      />
+    </div>
+  );
+}
---
+++ frontend/app/(flow)/test/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Flow from '../Flow'
+
+export default function TestPage() {
+  return (
+    <div><Flow/></div>
+  )
+}
