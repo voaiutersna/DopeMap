90fac1de57a03e68772b4e4fd86ddb08f752f507
Puttipong <163814251+ma2134@users.noreply.github.com>
2025-11-03 23:07:15 +0700

    new

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='h-12 bg-[#161616] w-full border-t border-[#3a3d3f] flex justify-center items-center text-zinc-200 font-mono'>
       <span className="text-xs sm:text-sm md:text-base">
-        Made with 💔🥀 by @guzamak & @whiskey & @Poociotv
+        Made with 💔🥀 by @guzamak & @whiskey & @Poociotv & @ma2134
       </span>
     </div>
   )
